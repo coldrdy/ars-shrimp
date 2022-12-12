b7aa9b38e8434592f5bf8b629c3a4dbb31aec528
umarhadi <hi@umarhadi.dev>
2022-12-12 23:26:41 +0700

    add @vercel/analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import "../styles/tailwind.css";
 import "../styles/slick.css";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
